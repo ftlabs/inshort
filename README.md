231f5cdab9ab447c2e4f380039cf4e0d398cea42
jdevt <33483633+jdevt@users.noreply.github.com>
2017-11-23 14:18:10 +0000

    Inital commit for the base API project

---
+++ README.md
@@ -0,0 +1,17 @@
+# FT Labs - Embellishr
+
+Interal application designed to help with the 'Word of the Year' feature.
+### Installation
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ npm install
+$ npm start
+```
+
+In another tab, run the following command to have webpack watch the client files.
+
+```sh
+$ npm run client-dev
+```
---
+++ test/index.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../index');
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('App authentication', () => {
+    it('Should respond with http: 200 & static page on / GET', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('Should redirect to use SSO if not a Allowed IP', (done) => {
+        chai.request(app)
+            .get('/')
+            .set('X-Forwarded-For', '192.168.1.1')         
+            .end((err, res) => {
+                expect(res).to.redirect;                
+                done();
+            });
+    });
+});
